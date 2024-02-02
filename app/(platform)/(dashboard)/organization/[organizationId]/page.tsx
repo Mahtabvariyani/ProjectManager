07778e845b5bbd07129443e8dfcb1988d241d9bb
Mahtabvariyani <mahtabvariyani@gmail.com>
2024-02-02 14:27:28 +0100

    new ages not finished

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,22 @@
 import { OrganizationSwitcher, auth } from "@clerk/nextjs"
+import { Info } from "./_components/info"
+import { BoardList } from "./_components/board-list"
+import { Suspense } from "react"
 
 
 const OrganizationIDPage = () => {
     const {userId, orgId} = auth()
-  return <div> id </div>
+  return (
+    <div className="w-full mb-20">
+    <Info isPro={isPro} />
+    {/* <Separator className="my-4" /> */}
+    <div className="px-2 md:px-4">
+      <Suspense fallback={<BoardList.Skeleton />}>
+        <BoardList />
+      </Suspense>
+    </div>
+  </div>
+  )
 }
 
 export default OrganizationIDPage
